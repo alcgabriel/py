5091918a65a62da7999adc6740dcb3ff1a2e9aef
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2025-12-26 22:55:19 -0300

    logic error

--- ch5/debugging.py
+++ ch5/debugging.py
@@ -1,2 +1,5 @@
 def take_magic_damage(health, resist, amp, spell_power):
-    pass
+    total_damage = spell_power * amp
+    real_damage = total_damage - resist
+    new_health = real_damage - health
+    return new_health
