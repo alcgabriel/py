d93043ce1d2ee55aca1f9ed5666829d797f1f40e
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2025-12-26 23:17:32 -0300

    complete

--- ch5/debuggingPratice.py
+++ ch5/debuggingPratice.py
@@ -1,4 +1,4 @@
 def unlock_achievement(before_xp, ach_xp, ach_name):
     after_xp = before_xp + ach_xp
-    alert = f"Achievement: {ach_name}"
+    alert = f"Achievement Unlocked: {ach_name}"
     return after_xp, alert
