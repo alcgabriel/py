60383bd7ff75a15e8dd93f3294c1d2709821ca75
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2025-12-26 22:41:04 -0300

    complete

--- ch5/unitTests.py
+++ ch5/unitTests.py
@@ -1,4 +1,4 @@
 def total_xp(level, xp_to_add):
     level_xp = level * 100
-    total_xp = xp_added + level_xp
-    return level_xp, total_xp
+    total_xp = xp_to_add + level_xp
+    return total_xp
