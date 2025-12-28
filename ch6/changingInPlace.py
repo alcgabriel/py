3cf70d84c0a43b800c10ae95fe1b50e01964a0c3
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2025-12-27 22:38:47 -0300

    complete but in a different way

--- ch6/changingInPlace.py
+++ ch6/changingInPlace.py
@@ -1,3 +1,3 @@
 def update_player_score(current_score, increment):
-    new_score = current_score + increment
-    return new_score
+    current_score = current_score + increment
+    return current_score
