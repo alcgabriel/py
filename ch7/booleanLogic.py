de88f34daab9ba4afece8f5c7276689e6ee6adc7
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-01-07 11:46:39 -0300

    complete

--- ch7/booleanLogic.py
+++ ch7/booleanLogic.py
@@ -1,3 +1,3 @@
 def does_attack_hit(attack_roll, armor_class):
-    return (attack_roll >= armor_class) and attack_roll >= 1 or (attack_roll == 20)
+    return (attack_roll >= armor_class) and attack_roll != 1 or (attack_roll == 20)
 
