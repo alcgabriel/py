96af444470037d9264b928bdec5aae599f98e1d7
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-01-07 21:40:50 -0300

    debug

--- ch7/shouldServeDrinks.py
+++ ch7/shouldServeDrinks.py
@@ -1,3 +1,3 @@
 def should_serve_customer(customer_age, on_break, time):
-    if customer_age >= 21 and time == True :
-        return "aa:w"
+    if customer_age >= 21 and time == True and time >= 5:
+        return "aa"
