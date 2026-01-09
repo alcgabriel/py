03019944eef375e947983c35bf91a02a60fe4a36
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-01-09 19:28:16 -0300

    complete

--- ch7/combatAdvantage.py
+++ ch7/combatAdvantage.py
@@ -1,6 +1,14 @@
 def combat_evaluation(player_power, enemy_defense):
     advantage, disadvantage, evenly_matched = False, False, False
 
-    # your code here
-
+    if player_power > enemy_defense:
+        advantage = True
+        return advantage, disadvantage, evenly_matched
+    elif player_power == enemy_defense:
+        evenly_matched = True
+        return advantage, disadvantage, evenly_matched
+    else: 
+        disadvantage = True
+        return advantage, disadvantage, evenly_matched
     return advantage, disadvantage, evenly_matched
+
