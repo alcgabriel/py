21ecea04882992766dfd8d4c099f70a0e49670cd
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-01-11 22:15:53 -0300

    complete

--- ch8/matchCountdown.py
+++ ch8/matchCountdown.py
@@ -1,6 +1,13 @@
 def countdown_to_start():
-    pass
-
+    countdown = 11
+    for i in range(10, 1, -1):
+        countdown -= 1
+        print(f"{countdown}...")
+        if countdown == 2:
+            countdown = 1
+            print(f"{countdown}...Fight!")
+            continue
+    return
 
 # Don't edit below this line
 
