7b3f4feb649984d889013a2f1bf660f4688318ea
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-01-12 00:21:11 -0300

    complete

--- ch8/meditate.py
+++ ch8/meditate.py
@@ -1,2 +1,10 @@
 def meditate(mana, max_mana, num_potions):
-    pass
+    while mana < max_mana:
+        mana += 1
+        num_potions -= 1
+        if mana == max_mana:
+            break
+        if num_potions == 0:
+            break
+    return mana, num_potions
+        
