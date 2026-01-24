7964ad39081ba39d21ad4e762799ababe1e8d533
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-01-23 23:39:57 -0300

    error

--- ch9/filterMessages.py
+++ ch9/filterMessages.py
@@ -1,2 +1,12 @@
 def filter_messages(messages):
-    pass
+    curse_words = []
+    count_words = []
+    for i in messages:
+        words = []
+        words = messages.split()
+        new_words = [] 
+        bad_words = []
+        print(words)
+        if "dang" in messages:
+            messages.join(bad_words)
+        print(words)
