fda12384a8f6fe42b7880f4553949954890af2b6
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-01-24 23:59:08 -0300

    error

--- ch9/filterMessages.py
+++ ch9/filterMessages.py
@@ -1,12 +1,17 @@
 def filter_messages(messages):
-    curse_words = []
-    count_words = []
-    for i in messages:
-        words = []
-        words = messages.split()
-        new_words = [] 
+    dang_msg = []
+    dang_count = []
+
+    for message in messages:
+        good_words = []
         bad_words = []
+        words = message.split()
+        for i in words: 
+            if words == ["dang"]:
+                bad_words.append([words[i]])
+            
+            return words, good_words, bad_words
+            dang_count = len(bad_words)
         print(words)
-        if "dang" in messages:
-            messages.join(bad_words)
-        print(words)
+
+    return dang_msg, dang_count
