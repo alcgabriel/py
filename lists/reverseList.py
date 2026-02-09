999de9a851fbeedb4c61f85c77320a7f26d6c041
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-02-09 11:58:44 -0300

    complete

--- lists/reverseList.py
+++ lists/reverseList.py
@@ -1,7 +1,5 @@
 def reverse_list(items):
     new_list = []
-    for i in range(len(items) -1 , -1, -1):
+    for i in range(len(items) -1, -1, -1):
         new_list = new_list + [items[i]]
-    
-        
     return new_list
