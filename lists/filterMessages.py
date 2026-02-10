e2f29de5214f6ac4201b4365217d56b7bd88e8f4
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-02-10 18:21:51 -0300

    progress
    Refactor filter_messages function to improve clarity and functionality.

--- lists/filterMessages.py
+++ lists/filterMessages.py
@@ -1,17 +1,27 @@
 def filter_messages(messages):
-    dang_msg = []
-    dang_count = []
-
-    for message in messages:
+    filtered_msg = []
+    filtered_count = []
+   # print(filtered_msg, filtered_count)
+    
+    for i in messages:
         good_words = []
-        bad_words = []
-        words = message.split()
-        for i in words: 
-            if words == ["dang"]:
-                bad_words.append([words[i]])
-            
-            return words, good_words, bad_words
-            dang_count = len(bad_words)
-        print(words)
-
-    return dang_msg, dang_count
+        dang_words = []
+        words = i.split()
+        #print(words)
+        for word in words:
+            # print(word)
+            if word == "dang":
+             #   print(word, 'bad')
+                dang_words.append(word)
+                print(dang_words, "badlist")
+               # dang_words.append(dang_words)
+                filtered_count.append(len(dang_words))
+            else: good_words.append(word)
+           # print(good_words, 'goodlist')
+    filtered_msg = " ".join(good_words)
+           # print(sentence)
+        
+    
+    filtered_count = len(dang_words)
+   # print(filtered_count)
+    return filtered_msg, filtered_count
