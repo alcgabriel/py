40fb2bdf80fa9c36480fc82c8a860654b7057243
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-02-23 11:13:22 -0300

    complete

--- lists/alchemyIngredients.py
+++ lists/alchemyIngredients.py
@@ -1,2 +1,15 @@
 def check_ingredient_match(recipe, inventory):
-    pass
+    missing_items = []
+    item_count = 0
+    for items in recipe:
+        if items not in inventory:
+           missing_items.append(items)
+        if items in inventory:
+            item_count = item_count + 1
+            
+    
+    percentage = item_count / len(recipe) * 100
+   
+    
+    
+    return percentage, missing_items
