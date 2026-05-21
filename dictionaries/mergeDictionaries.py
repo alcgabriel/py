efc2dde6bcf4da91a97e0fb204d0ce43b51bd539
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-05-20 23:54:15 -0300

    complete

--- dictionaries/mergeDictionaries.py
+++ dictionaries/mergeDictionaries.py
@@ -1,2 +1,12 @@
 def merge(dict1, dict2):
-pass
+
+    guild_merged = {
+        
+    }
+    for players in dict1:
+        guild_merged[players] = dict1[players]
+    for players in dict2:
+        guild_merged[players] = dict2[players]
+    for players in dict2:
+        print(guild_merged,"guild2")
+    return guild_merged
