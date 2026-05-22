2a664e8354bd0c5740328f74e2c418369f1185cb
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-05-21 21:31:16 -0300

    complete

--- sets/sets.py
+++ sets/sets.py
@@ -1,2 +1,10 @@
 def remove_duplicates(spells):
-pass
+    known_spells = set()
+    unique_spells = []
+    for spell in spells:
+        known_spells.add(spell)
+        print(known_spells)
+        if spell not in unique_spells:
+            unique_spells.append(spell)
+        print(unique_spells)
+    return(unique_spells)
