54e119e6290194ebc02ec7c352079ae5d80189e9
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-05-28 00:13:04 -0300

    complete

--- sets/setSubtraction.py
+++ sets/setSubtraction.py
@@ -1,2 +1,14 @@
 def find_missing_ids(first_ids, second_ids):
-pass
+    new_ids = set()
+    first_set = set()
+    second_set = set()
+    for id in first_ids:
+        first_set.add(id)
+        print(first_set)
+
+    for id in second_ids:
+        second_set.add(id)
+
+    new_ids = first_set - second_set
+    return new_ids
+    
