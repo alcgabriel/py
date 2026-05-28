b0e1fcacb8d1cce07f3598edc70c9c9d9fcc1e7e
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2026-05-28 00:03:48 -0300

    complete

--- sets/vowels.py
+++ sets/vowels.py
@@ -1,2 +1,9 @@
 def count_vowels(text):
-pass
+    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+    vowels_count = 0
+    unique_vowels = set()
+    for vowel in text:
+        if vowel in vowels:
+            vowels_count = vowels_count + 1
+            unique_vowels.add(vowel)
+    return vowels_count, unique_vowels
